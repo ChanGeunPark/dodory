46654714857fa013f2d3f3b61ccf3196e07b43bb
ChanGeunPark <design795@naver.com>
2022-02-08 22:17:48 +0900

    pubelising landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css'
+import { Navigation, Pagination } from 'swiper';
 
 
 const Home: NextPage = () => {
@@ -10,11 +11,10 @@ const Home: NextPage = () => {
   return (
     <div>
       <div className="px-4">
-        <div className='mx-auto max-w-[1250px] flex justify-end py-2 items-center'>
+        <div className='mx-auto max-w-[1250px] flex justify-end py-2 items-center space-x-5'>
           <Link href="/">
             <a className="text-sm">로그인</a>
           </Link>
-          <span className='mx-3'>|</span>
           <Link href="/">
             <a className="text-sm">회원가입</a>
           </Link>
@@ -74,8 +74,160 @@ const Home: NextPage = () => {
           </div>
         </article>
 
-        <article className="">
-        
+        <article className='mx-auto max-w-[1250px] flex mt-32 mb-32'>
+          <div className="w-1/3">
+            <h1 className="text-2xl font-semibold">도도리가 추천하는<br />이달의 행복!</h1>
+            <p className='text-zinc-500 my-5 font-medium'>이계절 나를 행복해해줄 당신만의 다육이에요!</p>
+            <div className='flex flex-wrap w-10/12'>
+              <span className='border border-zinc-400 px-5 py-2 mr-3 mb-3 rounded-full text-zinc-500 text-sm'>봄</span>
+              <span className='border border-zinc-400 px-5 py-2 mr-3 mb-3 rounded-full text-zinc-500 text-sm'>새로운 시작</span>
+              <span className='border border-zinc-400 px-5 py-2 mr-3 mb-3 rounded-full text-zinc-500 text-sm'>인테리어</span>
+              <span className='border border-zinc-400 px-5 py-2 mr-3 mb-3 rounded-full text-zinc-500 text-sm'>시원함</span>
+              <span className='border border-zinc-400 px-5 py-2 mr-3 mb-3 rounded-full text-zinc-500 text-sm'>소중한 시간</span>
+            </div>
+            <a href="#" className='bg-zinc-200 py-3 block w-52 text-center rounded-full hover:bg-indigo-600 transition-colors hover:text-white mt-8'>
+              다육이 더알아보기
+            </a>
+          </div>
+          <div className='w-2/3'>
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={20}
+              slidesPerGroup={1}
+              loop={true}
+              loopFillGroupWithBlank={true}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="mySwiper"
+              
+            >
+              <SwiperSlide>
+                <div className='h-96 overflow-hidden rounded-xl relative group'>
+                  <img src="/index/succulent1.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                  <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                    <div className='text-center'>
+                      <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                      <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                      <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='h-96 overflow-hidden rounded-xl relative group'>
+                  <img src="/index/succulent2.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                  <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                    <div className='text-center'>
+                      <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                      <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                      <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='h-96 overflow-hidden rounded-xl relative group'>
+                  <img src="/index/succulent3.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                  <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                    <div className='text-center'>
+                      <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                      <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                      <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='h-96 overflow-hidden rounded-xl relative group'>
+                  <img src="/index/succulent4.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                  <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                    <div className='text-center'>
+                      <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                      <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                      <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </article>
+
+        <article className='py-28 bg-[#edeee9]'>
+          <div className='max-w-[1250px] mx-auto'>
+            <h2 className="text-2xl font-semibold mb-1">새로 들어온 가족</h2>
+            <p className='font-medium text-zinc-500 mb-10'>오래 같이할수록 색이 예뻐지는 다육이와 함께하세요</p>
+            <div>
+              <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                slidesPerGroup={1}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+                
+              >
+                <SwiperSlide>
+                  <div className='h-72 overflow-hidden rounded-lg relative group'>
+                    <img src="/index/succulent1.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                    <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                      <div className='text-center'>
+                        <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                        <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                        <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='h-72 overflow-hidden rounded-lg relative group'>
+                    <img src="/index/succulent2.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                    <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                      <div className='text-center'>
+                        <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                        <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                        <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='h-72 overflow-hidden rounded-lg relative group'>
+                    <img src="/index/succulent3.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                    <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                      <div className='text-center'>
+                        <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                        <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                        <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='h-72 overflow-hidden rounded-lg relative group'>
+                    <img src="/index/succulent4.png" alt="succulent img" className='w-full h-full object-cover transition-all group-hover:blur-lg duration-200'/>
+                    <div className='absolute left-0 top-0 w-full h-full z-10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                      <div className='text-center'>
+                        <h2 className='text-white text-lg mb-3'>블루베리 다육이</h2>
+                        <p className='text-white text-sm'>블루베리처럼 둥글궁글한<br />나만의 귀여운 다육이</p>
+                        <h3 className='text-lg font-bold text-white mt-3'>30,000<span className='text-sm text-white font-normal'>원</span></h3>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+
+
+          </div>
         </article>
 
 
