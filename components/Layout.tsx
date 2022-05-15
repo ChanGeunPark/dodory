223406c618396036a9954c45d0fd47630d5ae49c
박찬근 <parkchangeun@bagchangeun-ui-MacBookPro.local>
2022-05-15 20:57:24 +0900

    3d작업 및 랜딩수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,8 @@ import useUser from "@libs/client/useUser";
 import React, { useContext, useRef } from "react";
 import { User } from "@prisma/client";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 interface LayoutProps {
   topAbsolute?: boolean;
@@ -14,28 +16,28 @@ interface LayoutProps {
 export default function Layout({ children, useFooter = true }: LayoutProps) {
   const { user } = useUser();
   const containerRef = useRef(null);
-
+  const router = useRouter();
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        // ... all available Locomotive Scroll instance options
-      }}
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      containerRef={containerRef}
-    >
-      <div data-scroll-container ref={containerRef}>
-        <Navbar user={user} />
-        <div>{children}</div>
+    <div data-scroll-container ref={containerRef}>
+      <Navbar user={user} />
+      {router.pathname !== "/" ? (
+        <div className="w-full h-[350px] bg-black overflow-hidden">
+          <img
+            src="/index/subBannerBg.jpg"
+            alt="subBanner"
+            className="w-full h-full object-cover opacity-70"
+          />
+          {/* <Image
+          src={"/index/subBannerBg.jpg"}
+          layout="fill"
+          className="object-cover"
+        /> */}
+        </div>
+      ) : null}
 
-        {useFooter ? <Footer /> : null}
-      </div>
-    </LocomotiveScrollProvider>
+      <div>{children}</div>
+
+      {useFooter ? <Footer /> : null}
+    </div>
   );
 }
